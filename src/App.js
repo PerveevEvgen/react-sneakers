87496ae9d34ecd4b531dca0eafb18562599e5722
Evgen Perveev <perveev.evgen@gmail.com>
2022-12-22 20:53:51 +0200

    fetch data from mockapi

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import Header from "./components/Header";
 import Card from "./components/Card";
 import Basket from "./components/Basket";
 import React from "react";
-import {BasketProvider} from "./components/Basket/basketContext";
-
-const arr = [
-  { name: "Мужские Кроссовки Nike Blazer Mid Suede", price: 1299 },
-  { name: "Мужские Кроссовки Nike Air Max 270", price: 4550 },
-  { name: "Мужские Кроссовки Nike Blazer Mid Suede", price: 16900 },
-  { name: "Кроссовки Puma X Aka Boku Future Rider", price: 10700 },
-];
-
+import { BasketProvider } from "./components/Basket/basketContext";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://63a492ae2a73744b007b9672.mockapi.io/items")
+      .then((fetchRes) => {
+        return fetchRes.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
+
   return (
     <div className="App clear">
       <div className="wrapper">
         <BasketProvider>
-          <Basket  />
-          <Header  />
+          <Basket />
+          <Header />
         </BasketProvider>
         <div className="cards_container d-flex flex-wrap justify-around">
-          {arr.map((item, index) => {
+          {items.map((item, index) => {
             return (
               <Card
                 imageUrl={`/img/sneakers/${index + 1}.jpg`}
